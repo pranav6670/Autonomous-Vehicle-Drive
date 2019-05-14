5980d7d652d8eaf769a7279024bcd1167e19d69c
Pranav <pranavnat24@gmail.com>
2019-05-14 10:24:55 +0530

    first commit

---
+++ RcDrive/arduino/rc_key_controller/rc_key_controller.ino
@@ -0,0 +1,82 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+// duration for output
+int time = 50;
+// initial command
+int command = 0;
+
+void setup() {
+  Serial.begin(115200);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setChannel(10);
+  radio.stopListening();
+}
+
+void loop() {
+  //receive command
+  if (Serial.available() > 0) {
+    command = Serial.read();
+  }
+  else {
+    reset();
+  }
+  send_command(command, time);
+}
+
+void right(int time) {
+  const char text[] = "right";
+  radio.write(&text, sizeof(text));
+
+}
+
+void left(int time) {
+  const char text[] = "left";
+  radio.write(&text, sizeof(text));
+
+}
+
+void forward(int time) {
+  const char text[] = "forward";
+  radio.write(&text, sizeof(text));
+
+}
+
+void reverse(int time) {
+  const char text[] = "reverse";
+  radio.write(&text, sizeof(text));
+
+}
+
+
+void reset() {
+  const char text[] = "stop";
+  radio.write(&text, sizeof(text));
+}
+
+void send_command(int command, int time) {
+  switch (command) {
+    //reset command
+    case 0: reset(); break;
+
+    // single command
+    case 1: forward(time); break;
+    case 2: reverse(time); break;
+    case 3: right(time); break;
+    case 4: left(time); break;
+
+    //combination command
+    //     case 6: forward_right(time); break;
+    //     case 7: forward_left(time); break;
+    //     case 8: reverse_right(time); break;
+    //     case 9: reverse_left(time); break;
+
+    default: Serial.print("Invalid Command\n");
+  }
+}
+
---
+++ RcDrive/arduino/rc_keyboard_bot/rc_keyboard_bot.ino
@@ -0,0 +1,120 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Adafruit_NeoPixel.h>
+
+#define in1 6
+#define in2 7
+#define in3 8
+#define in4 9
+
+RF24 radio(22, 23); // CE, CSN
+const byte address[6] = "00001";
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(8, 2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels1 = Adafruit_NeoPixel(3, 40, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(115200);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setChannel(10);
+  radio.startListening();
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pixels.begin();
+  pixels1.begin();
+}
+
+void loop() {
+  headlight();
+  //  if (!radio.available())
+  //    noRadio();
+  if (radio.available()) {
+    char text[32];
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+    if (strcmp("forward", text) == 0)
+      forward();
+    else if (strcmp("reverse", text) == 0)
+      reverse();
+    else if (strcmp("right", text) == 0)
+      right();
+    else if (strcmp("left", text) == 0)
+      left();
+    else if (strcmp("stop", text) == 0)
+      stop();
+    else {
+      Serial.print(text);
+      Serial.print("Don't come here");
+    }
+  }
+}
+void forward() {
+  Serial.println("forward");
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  setcolor(0, 255, 0);
+}
+void reverse() {
+  Serial.println("reverse");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  setcolor(255, 255, 255);
+}
+void right() {
+  Serial.println("right");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  setcolor(0, 255, 255);
+}
+void left() {
+  Serial.println("left");
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  setcolor(255, 0, 255);
+}
+void stop() {
+  Serial.println("stop");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  setcolor(255, 0, 0);
+}
+
+void setcolor(int x , int y, int z) {
+  for (int j = 0; j < 8; j++) {
+    pixels.setPixelColor(j, pixels.Color(x, y, z));
+    pixels.show();
+  }
+}
+void headlight() {
+  for (int j = 0; j < 3; j++) {
+    pixels1.setPixelColor(j, pixels1.Color(255, 255, 255));
+    pixels1.show();
+  }
+}
+//void noRadio() {
+//  for (int k = 0; k < 3; k++) {
+//    pixels.setPixelColor(k, pixels.Color(255, 255, 255));
+//    pixels.show();
+//  }
+//  delay(500);
+//  for (int k = 0; k < 3; k++) {
+//    pixels.setPixelColor(k, pixels.Color(0, 0, 0));
+//    pixels.show();
+//  }
+//  delay(500);
+//}
+
